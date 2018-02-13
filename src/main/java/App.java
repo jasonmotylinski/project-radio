49650fe5d44f5915b831626f6489d2e3a6379312
Jason Motylinski <jasonm@spotify.com>
2018-02-13 07:33:40 -0500

    feature: Added goal comment to main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
 public class App{
 
+  /*
+  Goal: Create a program which parses the file data/playlist.csv and finds the most popular
+  track on March 20, 2014
+   */
   public static void main(String args[]){
 
   }
